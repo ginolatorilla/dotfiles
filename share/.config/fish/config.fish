54551c46207fb6e3c4e5e422b641f198ee4da967
Gino Latorilla <pblatorilla+github@gmail.com>
2022-08-01 10:29:13 +0000

    Re-set EDITOR and VISUAL

--- share/.config/fish/config.fish
+++ share/.config/fish/config.fish
@@ -2,11 +2,11 @@ if grep -q WSL /proc/version
     # set --global --export DISPLAY (cat /etc/resolv.conf | grep -oP '(?<=nameserver ).+$'):0
     # set --global --export PYTHON_KEYRING_BACKEND keyring.backends.null.Keyring
     set --global --export BROWSER firefox
-    set --global --export EDITOR /usr/bin/vim.tiny
-    set --global --export VISUAL "/usr/bin/code --wait"
 end
 
 set --global --export GPG_TTY (tty)
+set --global --export EDITOR (command -v vim)
+set --global --export VISUAL (command -v code)' --wait'
 
 fish_add_path $HOME/.local/bin
 
