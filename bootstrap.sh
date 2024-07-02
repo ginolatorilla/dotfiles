a832bde900ba4da9fcb3883c83ef4054e58e86e0
Gino Latorilla <pblatorilla+github@gmail.com>
2024-07-02 23:46:18 +0800

    Bootstrap should use a Python virtual environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+PROJECT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+VENV_DIR="${PROJECT_DIR}/.venv"
 
-pip3 install -U --user ansible
-ansible-galaxy collection install ansible.utils
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv "$VENV_DIR"
+    "$VENV_DIR"/bin/pip install -r "$PROJECT_DIR"/requirements.txt
+fi
+
+source "$VENV_DIR"/bin/activate
+ansible-galaxy collection install ansible.utils community.general
