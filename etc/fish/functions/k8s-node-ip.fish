1887281ac20ccd27b8c2ac39cc375510457be033
Gino Latorilla <pblatorilla+github@gmail.com>
2025-03-13 12:20:58 +0800

    Delete node-ip and node-ssh fish functions

--- etc/fish/functions/k8s-node-ip.fish
+++
@@ -1,3 +0,0 @@
-function k8s-node-ip --description 'Gets the internal IP of a Kubernetes node'
-    kubectl get node/$argv[1] --output=json | jq -r '.status.addresses | [ .[] | select(.type == "InternalIP") ][0] | .address'
-end
--- etc/fish/functions/k8s-node-ssh.fish
+++
@@ -1,5 +0,0 @@
-function k8s-node-ssh --description 'SSH into a Kubernetes node'
-    string match -rq '^(?<userat>.+@)?(?<node>.+)$' $argv[1]
-    set -l SSH_EXTRA_ARGS '-oStrictHostKeyChecking=no'
-    ssh $userat$(k8s-node-ip $node) $argv[2..]
-end
